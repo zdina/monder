4160e3c5a7d6deb9384a94bbf4530f38f4d097fd
peterp <peter.pilgerstorfer@outlook.com>
2015-10-03 09:18:29 +0200

    modified train

--- ML/train.py
+++ ML/train.py
@@ -2,9 +2,13 @@ import sys
 import pyodbc
 import numpy as np
 import numpy.matlib
+from PIL import Image
+from StringIO import StringIO
 from sklearn import svm
 from sklearn.preprocessing import normalize
 import pickle
+
+import matplotlib.pyplot as plt
 
 # init data
 user = sys.argv[1]
@@ -12,30 +16,56 @@ print 'train data for user', user
 
 # load dataset
 print 'loading dataset'
-cnxn = pyodbc.connect('DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=localhost;DATABASE=monderdb;UID=dinazverinski;PWD=pg')
+
+driver = '{PostgreSQL Unicode}'
+if sys.platform == 'win32':
+	driver = '{PostgreSQL ODBC Driver(UNICODE)}'
+
+cnxn = pyodbc.connect(DRIVER=driver, SERVER='localhost', DATABASE='monderdb', UID='dinazverinski', PWD='pg')
 cursor = cnxn.cursor()
 
-cursor.execute("select * from person")
+n = cursor.execute("select movie.movie_id, movie.poster, opinion \
+	from user_movie join movie on user_movie.movie_id = movie.movie_id \
+	where user_id = ? and opinion < 2", user).rowcount
+#n = cursor.execute("select movie_id, poster from movie").rowcount
+f = 256;
 
-while 1:
+X = np.zeros((n,f))
+y = np.zeros(n)
+
+if n == 0:
+	exit()
+
+for i in range(n):
 	row = cursor.fetchone()
 	if not row:
 		break
-	print row
+	print row.movie_id
+	img = Image.open(StringIO(row.poster))
+	#plt.imshow(img)
+	#plt.show()
+		
+	# extract features
+	# histograms of images
+	print 'extracting features'
+	hist = np.histogram(img, bins=range(257), density=False)[0]
+	
+	X[i,:] = hist
+	y[i] = row.opinion
+	
+#print X
+#print y
 
-# extract features
-# histograms of images
-print 'extracting features'
-X = np.float64([[1,2,3], [2,2,0], [1,1,3], [1,2,4]])
-y = [1,0,1,1]
-
-f = int(np.size(X,1))
-n = int(np.size(X,0))
+if np.equal(y, np.zeros(n)).all() or np.equal(y, np.ones(n)).all():
+	exit()
+	
 
 # normalize features
 print 'normalizing features'
 factor = np.max(X,axis=1)
 X = X / np.transpose(np.matlib.repmat(factor,f,1))
+
+#print X
 
 # train classifier
 print 'train classifier'
@@ -43,7 +73,7 @@ print 'train classifier'
 classifier = svm.LinearSVC()
 classifier.fit(X,y)
 
-# print classifier.predict(X)
+print classifier.predict(X)
 # print classifier.predict([[1,1,0],[1,1,4]])
 
 # refresh user's classifier
